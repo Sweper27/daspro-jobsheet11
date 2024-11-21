2257a120af9f220689e8dea29d5ce966cd0a0760
Sweper27 <rifqiariessaputra@gmail.com>
2024-11-21 10:57:26 +0700

    menambah percobaan 5

---
+++ Percobaan626.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Percobaan626 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int p, l, t, luas, vol;
+
+        System.out.println("Masukkan panjang:");
+        p = input.nextInt();
+        System.out.println("Masukkan lebar:");
+        l = input.nextInt();
+        System.out.println("Masukkan tinggi:");
+        t = input.nextInt();
+
+        luas = p*l;
+        System.out.println("Luas Persegi Panjang adalah " + luas);
+        vol =t*p*l;
+        System.out.println("Volume Balok adalah " + vol);
+    }
+
+   
+    static int hitungLuas(int panjang, int lebar) {
+        int luas = panjang * lebar;
+        return luas;
+    }
+
+    static int hitungVolume(int tinggi, int panjang, int lebar) {
+        int volume = hitungLuas(panjang, lebar) * tinggi;
+        return volume;
+    }
+}
