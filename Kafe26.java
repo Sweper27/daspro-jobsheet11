eaacf1aaaa1aef9d7151af7d267c04ac39072dcf
Sweper27 <rifqiariessaputra@gmail.com>
2024-11-21 10:02:30 +0700

    menambah kan hasil percobaan 3

--- Kafe26.java
+++ Kafe26.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
 public class Kafe26 {
 
     public static void main(String[] args) {
-        Menu();
+        Scanner sc = new Scanner(System.in);
+        Menu("Andi", true, "DISKON50");
+
+        System.out.println("Masukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.println("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
     }
-    public static void Menu () {
+
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System.out.println("Selamat datang, " + namaPelanggan + ":");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid: Anda mendapatkan diskon 50%!");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid: Anda mendapatkan diskon 30%!");
+        } else {
+            System.out.println("Kode promo invalid. Tidak ada diskon tambahan.");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -13,5 +40,12 @@ public class Kafe26 {
         System.out.println("Mie Goreng - Rp 18,000");
         System.out.println("========================");
         System.out.println("Silakan pilih menu yang anda inginkan");
+    }
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000 };
+
+        int haraTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return haraTotal;
+        
     }
 }
